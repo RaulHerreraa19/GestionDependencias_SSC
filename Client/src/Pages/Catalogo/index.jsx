a2cab6eda620480cf26ab51631e822465ad47ae7
Juan Lozoya <jlozoya1@ucol.mx>
2025-08-25 21:52:39 -0600

    doing admin table

--- Client/src/Pages/Catalogo/index.jsx
+++ Client/src/Pages/Catalogo/index.jsx
@@ -12,6 +12,25 @@ export default function Catalogo(){
   return(
     <>
       <h1 className="text-center text-6xl text-[#669933] mt-5">Catalogo</h1>
+
+      <section className="w-[80%] m-[20px_auto]">
+        <table id="tablecgaf" className="w-full border-collapse">
+            <thead>
+              <tr className="bg-[#669933] text-white">
+                <th scope="col" className="p-[10px] border border-black">Nombre</th>
+                <th scope="col" className="p-[10px] border border-black">Cargo</th>
+              </tr>
+            </thead>
+            <tbody>
+                {/* {data.map((funcionario, index) => (
+                  <tr key={index} className="odd:bg-[#D9D9D9] even:bg-white">
+                    <td className="p-[10px] border border-black">{funcionario.nombre}</td>
+                    <td className="p-[10px] border border-black">{funcionario.cargo}</td>
+                  </tr>
+                ))} */}
+            </tbody>
+        </table>
+      </section>
     </>
   )
 }
--- Client/src/Pages/Funcionarios/index.jsx
+++ Client/src/Pages/Funcionarios/index.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { useOutletContext } from "react-router";
 import TablaCargos from "../../components/tablaCargos";
+import TablaAdministracion from "../../components/tablaAdministracion";
 import { getFuncinarios } from "@/api";
 
 const departamentos = [
@@ -64,26 +65,28 @@ export default function Funcionarios() {
     <>
       <h1 className="text-center text-6xl text-[#669933] mt-5">Funcionarios</h1>
 
-      {isLogged &&
-        <div className="flex items-center justify-end px-10 h-full">
-          <button className="cursor-pointer px-4 py-2 bg-[#669933] rounded-md text-white">Editar Tabla</button>
+      {isLogged ?
+        <div>
+          <TablaAdministracion data={funcionarios}/>
+        </div>
+        :
+         <div>
+          <nav className="department-nav">
+            <ul className="flex justify-self-start ml-[152px] list-none p-0 gap-6 mt-24 ">
+                {departamentos.map((departamento) => (
+                  <li key={departamento.param}>
+                    <button
+                      onClick={() => getFuncionarios(departamento.param)}
+                      className="inline-block no-underline text-[#52752f] bg-transparent border border-transparent rounded px-3 py-2.5 cursor-pointer hover:text-[#669933] hover:border-gray-300"
+                    >{departamento.nombre}</button>
+                  </li>
+                ))}
+            </ul>
+          </nav>
+          
+          <TablaCargos data={ funcionarios } />
         </div>
       }
-
-      <nav className="department-nav">
-        <ul className="flex justify-self-start ml-[152px] list-none p-0 gap-6 mt-24 ">
-            {departamentos.map((departamento) => (
-              <li key={departamento.param}>
-                <button
-                  onClick={() => getFuncionarios(departamento.param)}
-                  className="inline-block no-underline text-[#52752f] bg-transparent border border-transparent rounded px-3 py-2.5 cursor-pointer hover:text-[#669933] hover:border-gray-300"
-                >{departamento.nombre}</button>
-              </li>
-            ))}
-        </ul>
-      </nav>
-
-      <TablaCargos data={ funcionarios } />
     </>
   );
 }
